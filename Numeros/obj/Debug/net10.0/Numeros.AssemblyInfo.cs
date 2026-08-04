ed357c39ee83f9598197ff228aaccae7e5f2715e
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-08-04 14:17:24 -0300

    Testes

--- Numeros/obj/Debug/net10.0/Numeros.AssemblyInfo.cs
+++ Numeros/obj/Debug/net10.0/Numeros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd1870ef8fa9b7aa409cb369c426f2e30db1b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
