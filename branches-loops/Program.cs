d54a8071b28497f52e306567c699912c00f3e68b
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-08-26 16:29:36 -0300

    brancheseloop

--- branches-loops/Program.cs
+++ branches-loops/Program.cs
@@ -1,7 +1,6 @@
 ﻿int a = 5;
-int b = 6;
-//int b = 3;
-if (a + b > 10)
-    Console.WriteLine("A resposta é maior que 10");
-else
-    Console.WriteLine("A resposta não é maior que 10");
+int b = 3;
+int c = 4;
+
+
+if ((a + b + c > ))
