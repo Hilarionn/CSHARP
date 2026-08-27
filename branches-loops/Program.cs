0f8e861adc6331a22d304431fada4edd72bae099
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-08-27 09:39:28 -0300

    Teste

--- branches-loops/Program.cs
+++ branches-loops/Program.cs
@@ -3,4 +3,7 @@ int b = 3;
 int c = 4;
 
 
-if ((a + b + c > ))
+if ((a + b + c > 10))
+    Console.WriteLine("A resposta é maior que 10");
+else
+    Console.WriteLine("A resposta não é maior que 10");
--- branches-loops/obj/Debug/net10.0/branches-loops.AssemblyInfo.cs
+++ branches-loops/obj/Debug/net10.0/branches-loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("branches-loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f542c530b83e699ceb7bfb192245361ecf89e62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54a8071b28497f52e306567c699912c00f3e68b")]
 [assembly: System.Reflection.AssemblyProductAttribute("branches-loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("branches-loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
