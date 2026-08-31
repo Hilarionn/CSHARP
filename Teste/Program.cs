d9da722b3f7c6c84f76ee6745b47f13b326b847e
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-08-31 16:48:15 -0300

    Testes

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -3,3 +3,9 @@
 Console.WriteLine("Número de telefone:");
 
 var telefone = (A: 3, B: 1, C:9,  D:9,  E:1 , F:1,  G:8, H:4,  I:5,  J:9,  K:9);
+
+
+for (
+    int num1 = 0; num1 < telefone.A ; num1 ++
+
+);
--- Teste/obj/Debug/net10.0/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net10.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f379c7aa261f120d78935992d712da1dd1d64e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd861dcd7bfc8ff6457056eeaf11cc3b70163d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
