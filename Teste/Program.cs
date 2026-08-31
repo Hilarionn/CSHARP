bd861dcd7bfc8ff6457056eeaf11cc3b70163d1d
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-08-31 13:59:54 -0300

    testes

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Console.WriteLine("Número de telefone:");
+
+var telefone = (A: 3, B: 1, C:9,  D:9,  E:1 , F:1,  G:8, H:4,  I:5,  J:9,  K:9);
