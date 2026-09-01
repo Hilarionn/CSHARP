2e5dfcfc9b286dc41c101571f1a983d37f014717
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-09-01 13:09:09 -0300

    piramide

---
+++ Piramides/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+// IMPRIMIR NA TELA UMA PIRÂMIDE, COM UM VALOR 5 DE BASE
+// IMPRIMIR NA TELA UMA PIRÂMIDE INVERTIDA, COM TOPO 5
+// DEIXAR O USUÁRIO ESCOLHER O NÚMERO DA BASE
+//PIRÂMIDE EXATA, UTILIZANDO ESPAÇO
+int y = 5;
+int x = 0;
+for (x = 0; x < y ; x++)
+{
+    Console.WriteLine($"{x}");
+}
